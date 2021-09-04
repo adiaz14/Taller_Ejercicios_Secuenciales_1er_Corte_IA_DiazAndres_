9a0501f95f78d76fce83aae5ab099d471c1bca14
Andres Diaz <adiaz14@live.com>
2021-09-04 15:48:41 -0500

    Segundo commit

--- Taller_Ejercicios_Secuenciales_1er_Corte_IA_DiazAndres_.py
+++ Taller_Ejercicios_Secuenciales_1er_Corte_IA_DiazAndres_.py
@@ -2,6 +2,12 @@
 """
 Created on Sat Sep  4 15:37:54 2021
 
-@author: Andres Diaz
+@author: Andrés Díaz Ortega
 """
 
+# Calcule el valor de Y indicando paso a paso como llegó al resultado
+
+# 1. y=((5+2-5)^2*5+8/2-30)/2*5-3
+
+y = ((5 + 2 - 5) ** 2 * 5 + 8 / 2 - 30) / 2 * 5 - 3
+print(f'El valor de y es: {y}')
