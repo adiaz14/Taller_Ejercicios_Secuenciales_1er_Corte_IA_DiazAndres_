8095e893a58c4db279a32ced27f743946b0eb8b6
Andres Diaz <adiaz14@live.com>
2021-09-04 15:57:37 -0500

    Septimo Commit

--- Taller_Ejercicios_Secuenciales_1er_Corte_IA_DiazAndres_.py
+++ Taller_Ejercicios_Secuenciales_1er_Corte_IA_DiazAndres_.py
@@ -55,6 +55,32 @@ edad = int(input('Digite la edad de la persona: '))
 num_pulsaciones = (200 - edad) / 10
 print(f'El número de pulsaciones de la persona es: {num_pulsaciones}')
 
+# 3. Tres personas deciden invertir su dinero para fundar una empresa.
+# Cada una de ellas invierte una cantidad distinta. Obtener el porcentaje
+# que cada quien invierte con respecto a la cantidad total invertida.
+
+print('\n------------- DATOS PARA EL REGISTRO DE INVERSIONES -------------\n')
+inversion_persona1 = float(input('Digite la inversión de la 1ra persona: '))
+inversion_persona2 = float(input('Digite la inversión de la 2da persona: '))
+inversion_persona3 = float(input('Digite la inversión de la 3er persona: '))
+
+total_inversion = inversion_persona1+inversion_persona2+inversion_persona3
+
+porcentaje_persona1 = (inversion_persona1/total_inversion)*100
+porcentaje_persona2 = (inversion_persona2/total_inversion)*100
+porcentaje_persona3 = (inversion_persona3/total_inversion)*100
+
+print('\n-------------- RESUMEN DE REGISTRO DE INVERSIONES --------------\n')
+print('----- Datos primera persona -----')
+print(f'Valor de la inversión: ${inversion_persona1:,}')
+print(f'Porcentaje de participacion: {porcentaje_persona1}%\n')
+print('----- Datos segunda persona -----')
+print(f'Valor de la inversión: ${inversion_persona2:,}')
+print(f'Porcentaje de participacion: {porcentaje_persona2}%\n')
+print('----- Datos tercera persona -----')
+print(f'Valor de la inversión: ${inversion_persona3:,}')
+print(f'Porcentaje de participacion: {porcentaje_persona3}%')
+
 
 
 
